95161db47fa20e942c260a969fb6bdfc71f537fa
StackDoubleFlow <ojaslandge@gmail.com>
2021-01-29 19:58:58 -0600

    Intial Commit

---
+++ build.rs
@@ -0,0 +1,7 @@
+
+fn main() {
+    cc::Build::new()
+        .file("And64InlineHook/And64InlineHook.cpp")
+        .include("And64InlineHook")
+        .compile("inline_hook");
+}
---
+++ src/lib.rs
@@ -0,0 +1,10 @@
+
+#[no_mangle]
+pub extern fn setup() {
+
+}
+
+#[no_mangle]
+pub extern fn load() {
+
+}
